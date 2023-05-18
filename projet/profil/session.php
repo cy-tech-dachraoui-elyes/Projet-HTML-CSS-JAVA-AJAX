c3ba077340429975bc9403b721c02d79c608c700
swiczkaada <124392323+swiczkaada@users.noreply.github.com>
2023-05-18 20:48:59 +0200

    Create session.php

---
+++ projet/profil/session.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if(isset($_SESSION['user'])){
+    // Récupérer les données des utilisateurs depuis le fichier JSON
+    $users = json_decode(file_get_contents("../utilisateurs.json"), true);
+
+    // Trouver l'utilisateur connecté dans le tableau des utilisateurs
+    foreach($users['jeune'] as $user){
+        if($user['id'] === $_SESSION['user']){
+            // Afficher les informations de l'utilisateur
+            $id = $user['id'];
+            $prenom = $user['prenom'];
+            $nom = $user['nom'];
+            $email = $user['email'];
+            $date = $user['date'];
+            $reseau = $user['reseau'];
+            $tel = $user['tel'];
+            $mdp = $user['mdp'];
+        }
+    }
+} else {
+    // Si l'utilisateur n'est pas connecté, le rediriger vers la page de connexion
+    //header("Location: connexion.php");
+    //exit;
+    var_dump($_SESSION);
+}
+?>
