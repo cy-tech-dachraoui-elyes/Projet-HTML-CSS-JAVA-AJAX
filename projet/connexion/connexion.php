6f32756cf92fe08a3b32827da83bcf4c825d8dcd
swiczkaada <124392323+swiczkaada@users.noreply.github.com>
2023-06-11 00:28:00 +0200

    Update connexion.php

--- projet/connexion/connexion.php
+++ projet/connexion/connexion.php
@@ -1,40 +1,37 @@
 <?php
 session_start();
 
-if(isset($_POST['email']) && isset($_POST['mdp'])){
+if (isset($_POST['email']) && isset($_POST['mdp'])) {
     $email = strtolower($_POST['email']);
     $mdp = $_POST['mdp'];
 
     // Récupérer les données des utilisateurs depuis le fichier JSON
-    $users = json_decode(file_get_contents("../utilisateurs.json"), true);
+    $utilisateurs = json_decode(file_get_contents("../utilisateurs.json"), true);
 
     // Vérifier si les informations de connexion sont correctes
-    foreach($users['jeune'] as $user){
-        if($user['email'] === $email && $user['mdp'] === $mdp ){
-            $_SESSION['user'] = $user['id'];
+    foreach ($utilisateurs['jeune'] as $utilisateur) {
+        if ($utilisateur['email'] === $email && $utilisateur['mdp'] === $mdp) {
+            $_SESSION['user'] = $utilisateur['id'];
             header("Location: /profil/profil.php");
             exit;
         }
-         //else {
-            //var_dump($email);
-            //var_dump($user['email']);}
-    
-    
-    } 
-   // Si les informations de connexion sont incorrectes, afficher un message d'erreur[{"nom":"sw","prenom":"ss","email":"a@gmail.com","date":"2023-05-15","reseau":"Instagram","mdp":"ok"}]
-        $msg = "Adresse email ou mot de passe incorrect";
+    }
 
+    // Si les informations de connexion sont incorrectes, afficher un message d'erreur
+    $msg = "Adresse email ou mot de passe incorrect";
 }
 ?>
 
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Connexion</title>
     <link href="connexion.css" type="text/css" rel="stylesheet">
-    <script src="../fonction.js" type="text/javascript"> </script>
+    <script src="../fonction.js" type="text/javascript"></script>
 </head>
+
 <body>
     <div class="bande-grise">
         <div class="contenu">
@@ -47,51 +44,49 @@ if(isset($_POST['email']) && isset($_POST['mdp'])){
         </div>
     </div><br>
 
-   
-
+    <div class="Accueil">
+        <a href="../accueil.html" class="button">Retour Accueil</a>
+    </div>
+    
     <form method="POST">
-        <div class="gradient"></div>
-        <div class="sign-up-wrapper">
-            <div class="sign-up">
-                <?php if(isset($msg)): ?> <!-- Affichage d'un message d'erreur en cas d'identifiant ou de mot de passe incorrect.-->
-                <p class="erreur"><?php echo $msg; ?></p> 
+        <div class="zone-inscription">
+            <div class="inscription">
+                <?php if (isset($msg)): ?>
+                    <!-- Affichage d'un message d'erreur en cas d'identifiant ou de mot de passe incorrect. -->
+                    <p class="erreur"><?php echo $msg; ?></p>
                 <?php endif; ?>
                 <h2>Se connecter</h2>
-                <div class="input-box">
+                <div class="champ-saisie">
                     <span>Email</span>
                     <img class="imgs" width="16px" height="16px" src="/boutton/email.png" alt="email">
                     <input type="email" class="input" placeholder="Entre ton email..." id="email" name="email" required>
                 </div>
-                <div class="input-box">
+                <div class="champ-saisie">
                     <span>Mot de passe</span>
                     <img class="imgs" width="16px" height="16px" src="/boutton/lock.png" alt="password" onclick="affichage()">
                     <input type="password" class="input" placeholder="Entre ton mot de passe..." id="mdp" name="mdp" required>
                 </div>
                 <input type="submit" value="Se connecter">
 
-                <div class="container">
-                    <span class="text">Pas de compte? </span><span class="text"><a href="/inscription/Inscription.html">Créez-en un !</a></span>
+                <div class="compte">
+                    <span class="texte">Pas de compte? </span><span class="texte"><a href="/inscription/Inscription.html">Créez-en un !</a></span>
                 </div>
-        <img src="/image/logo_jeune.png" alt="Image" class="dec">
+            </div>
+        </div>
     </form>
-    <div class="trait-long">
-    
-</div>
 
-    
     <style>
-        
-        /*Style CSS qui met en rouge le message "adresse  ou mot de passe incorrect" 
-        pour signaler une erreur à l'utilisateur*/
+        /* Style CSS qui met en rouge le message "adresse ou mot de passe incorrect" 
+           pour signaler une erreur à l'utilisateur */
 
-        .erreur{
-            position  : absolute;
-            top : 170px;
+        .erreur {
+            position: absolute;
+            top: 170px;
             color: red;
             font-size: 0.9em;
             padding: 0.5em;
         }
-
     </style>
 </body>
+
 </html>
