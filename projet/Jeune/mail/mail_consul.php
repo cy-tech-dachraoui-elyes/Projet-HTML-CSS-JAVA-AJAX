9e584b7b1b5a77de525f7b55fc7074047943d654
ManlB <102758137+ManlB@users.noreply.github.com>
2023-06-10 18:27:45 +0200

    Update mail_consul.php

--- projet/Jeune/mail/mail_consul.php
+++ projet/Jeune/mail/mail_consul.php
@@ -4,6 +4,39 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+
+////Traitement des références cochées
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_POST['id'];
+        $referencesSelectionnees = $_POST["reference"];
+        
+        // Mettre à jour le fichier JSON avec les références modifiées
+        $jsonString = file_get_contents('../utilisateurs.json');
+        $data = json_decode($jsonString, true);
+        
+        // Parcourir les utilisateurs et mettre à jour les références
+        foreach ($data["jeune"] as &$jeune) {
+            if ($jeune['id'] == $id) {
+                foreach ($jeune["references"] as &$reference) {
+                    // Vérifier si l'email de la référence correspond à l'email de la référence sélectionnée
+                    if (in_array($reference["email_ref"], $referencesSelectionnees)) {
+                        $reference["cochee"] = true; // Marquer la référence comme sélectionnée
+                    } else {
+                        $reference["cochee"] = false; // Marquer la référence comme non sélectionnée
+                    }
+                }
+            }
+        }
+        
+        // Enregistrer les modifications dans le fichier JSON
+        $updatedJsonString = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE );
+        file_put_contents('../utilisateurs.json', $updatedJsonString);
+        
+
+}
+
+
 // Création d'une nouvelle instance de PHPMailer
 $mail = new PHPMailer(true);
 
