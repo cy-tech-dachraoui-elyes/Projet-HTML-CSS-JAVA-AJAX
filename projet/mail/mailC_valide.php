f994ff1850cfe403e2376f38a3c00e3b4259cb1b
swiczkaada <124392323+swiczkaada@users.noreply.github.com>
2023-06-11 20:38:28 +0200

    Update mailC_valide.php

--- projet/mail/mailC_valide.php
+++ projet/mail/mailC_valide.php
@@ -11,10 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = htmlspecialchars(strip_tags($_POST['email'])) ?? '';
     $nom = htmlspecialchars(strip_tags($_POST['nom'])) ?? '';
     $prenom = htmlspecialchars(strip_tags($_POST['prenom'])) ?? '';
+    $message = htmlspecialchars(strip_tags($_POST['message'])) ?? '';
 
     $email_encoded = urlencode($email);
-    $objet = utf8_decode('Validation recrutement');
-
+    $objet = utf8_decode('Refus référence');
 try {
     // Paramètres du serveur SMTP de Laposte
     $mail->isSMTP();
@@ -33,10 +33,8 @@ try {
     $mail->Subject = 'Jeune.engagement6.4';
     $mail->Body = utf8_decode('Bonjour ' . $prenom . ", \n\n" .
 
-    "Nous sommes ravis de vous informer que votre candidature a été retenue pour passer à la prochaine étape du processus de sélection.\n\n" .
-    "Nous avons examiné attentivement votre profil, vos qualifications et votre expérience, et nous sommes convaincus que vous possédez les compétences et les aptitudes nécessaires pour contribuer de manière significative à notre équipe.\n".
-    "Nous aimerions poursuivre le processus de sélection en organisant une entrevue avec vous. Nous vous contacterons dans les prochains jours pour convenir d'une date et d'une heure qui vous conviennent.\n".
-    "Pendant l'entretien, nous discuterons plus en détail de votre expérience, de vos compétences et de la manière dont vous pourriez contribuer à notre équipe.\n".
+    "Nous regrettons de vous informer que votre demande de référence a été refusée pour le motif suivant:\n\n" .
+    "'". $message ."'\n\n" .
     "Si vous avez des questions ou souhaitez obtenir de plus amples informations, n'hésitez pas à nous contacter.\n\n".
     "Cordialement,\n" .
     "L'équipe Jeunes 6.4");
@@ -45,7 +43,8 @@ try {
 
     $mail->send();
     //echo 'L\'e-mail a été envoyé avec succès';
-    header('Location: ../page0.html');
+    header('Location: /Referent/refus.html');
+    
 
 } catch (Exception $e) {
     echo 'Une erreur s\'est produite lors de l\'envoi de l\'e-mail : ' . $mail->ErrorInfo;
