7e76af36d6cf68575c08d7e04ffd7b4ec2a3c9a1
swiczkaada <124392323+swiczkaada@users.noreply.github.com>
2023-06-11 22:02:45 +0200

    Update mail_consul.php

--- projet/mail/mail_consul.php
+++ projet/mail/mail_consul.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $emailC_encoded = urlencode($emailC);
     $id_encoded = urlencode($id);
-    $lien = "http://localhost:8080/consultant/consultant.php?emailC=$emailC_encoded&id=$id_encoded";
+    $lien = "http://localhost:8080/Consultant/consultant.php?emailC=$emailC_encoded&id=$id_encoded";
     $nomC = 'ent';
     $prenomC ='consultant';
     $objet = utf8_decode('Demande Jeune');
