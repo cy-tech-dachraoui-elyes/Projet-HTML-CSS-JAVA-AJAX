da3ba788acabb88b2aff3c789dd69c77e352a4f4
swiczkaada <124392323+swiczkaada@users.noreply.github.com>
2023-06-11 09:42:26 +0200

    Update Inscription.js

--- projet/inscription/Inscription.js
+++ projet/inscription/Inscription.js
@@ -1,15 +1,16 @@
 document.getElementById("inscription").addEventListener("submit", function(e) {
-	e.preventDefault();
+	e.preventDefault(); 
  
-	var data = new FormData(this);
+	var data = new FormData(this); // Crée un objet FormData pour collecter les données du formulaire
  
-	var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest(); // Crée un objet XMLHttpRequest pour effectuer des requêtes HTTP
  
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			//console.log(this.response);
 			window.location.href = "/connexion/connexion.php";
 		} else if (this.readyState == 4) {
+			// La requête a été terminée, mais il y a eu une erreur
 			alert("Une erreur est survenue...");
 		}
 	};
@@ -19,6 +20,6 @@ document.getElementById("inscription").addEventListener("submit", function(e) {
 	// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send(data);
  
-	return false;
+	return false; // Empêche le formulaire de se soumettre normalement
 	
 });
